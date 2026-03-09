720c0b06367682ea721e514d2960ddc98e559f31
Tino <tinomavropulos1@gmail.com>
2026-03-09 10:22:58 +0100

    Vite config fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,14 @@ export default defineConfig(({ command }) => ({
   server: {
     host: true
   },
-  base: command === 'build' ? '/Ecommerce-Academy-Assignment/' : '/'
+  base: command === 'build' ? '/Ecommerce-Academy-Assignment/' : '/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        productPage: 'pages/productPage.html',
+        cartPage: 'pages/cartPage.html',
+      }
+    }
+  }
 }))
